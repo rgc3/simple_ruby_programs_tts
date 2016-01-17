262177c1fd61fe945e339b67474c6ba2e8276a4e
Robert Cunningham <rgc3dev@gmail.com>
2016-01-17 17:02:42 -0500

    added more functionality to guessing game but new feature not working like I want

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,8 +1,10 @@
-randomnumber = rand(10)+1
-puts 'Please pick a number from 1 - 10'
+randomnumber = rand(100)+1
+puts 'Please pick a number from 1 - 100'
 number = gets.chomp.to_i
 if number == randomnumber
 	puts 'Wow! You guessed correctly!'
+#elsif number == 5 > randomnumber || 5 < randomnumber
+	#puts 'Oh! So close.'
 else
 	puts 'Nope! That is not the number.'
 end
