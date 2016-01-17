487c3864fb7439ab9f15565bd66402d65061d39b
Robert Cunningham <rgc3dev@gmail.com>
2016-01-17 16:46:09 -0500

    added more animals and sounds to dogsays_catsays.rb

--- dogsays_catsays.rb
+++ dogsays_catsays.rb
@@ -1,9 +1,17 @@
-puts 'Dog or cat?'
+puts 'Name an animal'
 answer = gets.chomp.downcase
 if answer == 'dog'
 	puts 'Dog goes ruff'
 elsif answer == 'cat'
 	puts 'Cat goes meow'
+elsif answer == 'cow'
+	puts 'Cow goes mooo'
+elsif answer == 'donkey'
+	puts 'Donkey goes hee-haw'
+elsif answer == 'bird'
+	puts 'Bird goes chirp chirp'
+elsif answer == 'turkey'
+	puts 'Turkey goes gobble gobble'
 else
-	puts "Nope!"
+	puts "I'm sorry, I don't know that animal."
 end
