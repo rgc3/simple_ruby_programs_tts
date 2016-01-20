4819adc796b558714d02bfbe46ef4e89f00e866f
Robert Cunningham <rgc3dev@gmail.com>
2016-01-20 08:53:11 -0500

    timeofday.rb homework solutions

--- timeofday.rb
+++ timeofday.rb
@@ -1,1 +1,26 @@
-puts 'What time is it?'
+#I did not get this one because I made it more complicated than need to be.
+#I thought I needed to put times but really only needed morning, afternoon and evening or night
+
+#below is Bryants solution 
+# puts 'What time is it?'
+# answer = gets.chomp.to_i
+# case answer
+# when answer >= 6 && answer <= 11
+# 	puts 'Good morning!'
+# when answer >= 12 && answer <= 19
+# 	puts 'Good afternoon!'
+# else 
+# 	puts 'Good night!'
+# end
+
+#below is Marty's solution
+# puts 'What time is it? Please use military time.'
+# answer = gets.chomp.to_i
+# case answer
+# when 0..1200
+# 	puts 'Good morning'
+# when 1300..1600
+# 	puts 'Good morning.'
+# else
+# 	puts 'Good night.'
+# end
