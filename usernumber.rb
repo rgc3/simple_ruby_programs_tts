9a7d89997d1dbcb68733a8a56c4d8ccd4cd29879
Robert Cunningham <rgc3dev@gmail.com>
2016-01-20 09:10:54 -0500

    usernumber.rb homework solution

--- usernumber.rb
+++ usernumber.rb
@@ -1,6 +1,19 @@
-puts "Please provide me with a number"
-number = gets.chomp.to_i
+#below is all I could come up with
+# puts "Please provide me with a number"
+# number = gets.chomp.to_i
 
-until number == "done"
+# until number == "done"
 
+# end
+
+#Walid's solution
+response = ''
+newnum = 0
+until response == 'done'
+	puts 'Give me some numbers, type done when you want'
+	response = gets.chomp
+	if response != 'done'
+		addition = response.to_i
+		puts newnum += addition
+	end
 end
