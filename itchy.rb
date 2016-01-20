0820d948cd79b705bca8028ff3302914b5401430
Robert Cunningham <rgc3dev@gmail.com>
2016-01-20 08:15:13 -0500

    homework solution for itchy.rb

--- itchy.rb
+++ itchy.rb
@@ -1,7 +1,18 @@
-please = 'Dad, can we go to Itchy and Scratchy land?'
-puts please
+
+#my version of homework, problem being it repeats the question after dad answers yes
+
+#please = 'Dad, can we go to Itchy and Scratchy land?'
+#puts please
+#answer = ''
+#until answer == 'yes'
+#answer = gets.chomp.downcase
+#puts please
+#end
+
+
+#Simplified version that works correctly
 answer = ''
 until answer == 'yes'
-answer = gets.chomp.downcase
-puts please
+	puts 'Dad, can we go to Itchy and Scratchy land?'
+	answer = gets.chomp.downcase
 end
