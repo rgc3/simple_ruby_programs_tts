2a6cb5aa76c6e72001ec6f839784dade935a3a82
Robert Cunningham <rgc3dev@gmail.com>
2016-01-20 08:38:25 -0500

    repeat.rb homework solution

--- repeat.rb
+++ repeat.rb
@@ -1,6 +1,16 @@
+#there is an extra line after the 'Please type something.' string
+#puts 'Please type something.'
+#response = ''
+#while response.downcase != 'i\'m a dummy'
+#puts response
+#response = gets.chomp
+#end
+
+
+# switching the two lines above end removes extra line
 puts 'Please type something.'
 response = ''
 while response.downcase != 'i\'m a dummy'
-puts response
 response = gets.chomp
+puts response
 end
