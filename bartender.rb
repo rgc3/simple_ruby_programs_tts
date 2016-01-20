95d322ee4a8f76e62d68f470901c5eb2a839ed03
Robert Cunningham <rgc3dev@gmail.com>
2016-01-20 09:00:20 -0500

    bartender.rb homework solution

--- bartender.rb
+++ bartender.rb
@@ -1,7 +1,17 @@
+#below is my solution that did not work
 puts 'Hello, how old are you?'
 age = gets.chomp.to_i
 if age < 21
 	puts 'You have ' + (21 - age) + 'years until you can legally drink.'
 else
 	puts 'What would you like to drink?'
+end
+
+#Walid's solution below
+puts 'How old are you?'
+age = gets.chomp.to_i
+if age >= 21
+	puts 'Have a drink!'
+else 
+	puts "You still have #{21 - age} years left."
 end
