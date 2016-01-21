9e9288cf40d4748fa2bdb2a789c67bb7227e5092
Robert Cunningham <rgc3dev@gmail.com>
2016-01-21 10:18:11 -0500

    Solution to problem

--- fivenumbersdoteach.rb
+++ fivenumbersdoteach.rb
@@ -6,3 +6,19 @@
 
 puts 'Please provide me with a number'
 num = gets.chomp.to_i
+
+
+
+# Solution below
+
+array = []
+puts 'Give me 5 numbers'
+5.times do input = gets.chomp.to_i
+	array.push input
+end
+
+puts array.inject(:+)
+puts array.inject(:*)
+puts array.max
+puts array.min
+puts array
