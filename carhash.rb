d9aaaa4bd300e196f1ef0b26d05bad0dfb61ec8d
Robert Cunningham <rgc3dev@gmail.com>
2016-01-21 10:05:18 -0500

    add class excercises carhash.rb and fivenumbersdoteach.rb

---
+++ carhash.rb
@@ -0,0 +1,19 @@
+# Create a hash of several cars:
+# The make is the Key, the model is the Value.
+# Ask the user what car (model) they drive,
+# use the Hash to determine what make it is.
+# (e.g., "Oh, you drive a Civic? That's a Honda, right?")
+
+
+# My attempt below
+
+cars = {'Chevy' => 'Corvette', 'Ford' => 'Galaxy', 'Dodge' => 'Charger', 'Nissan' => 'GTR'}
+puts 'What model car do you drive?'
+model = gets.chomp.capitalize
+if name == #{cars[Chevy]}
+
+puts "Oh, you drive a #{}? That's a #{}, right?"
+
+
+
+# Solution below
---
+++ fivenumbersdoteach.rb
@@ -0,0 +1,8 @@
+# Ask the user for 5 numbers,
+# store them in an array,
+# then find the sum, product, largest, and smallest of those numbers.
+
+# My attempt below
+
+puts 'Please provide me with a number'
+num = gets.chomp.to_i
