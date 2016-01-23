214f394c73c8dc0e953b11cfaaebb61a19cedb20
Robert Cunningham <rgc3dev@gmail.com>
2016-01-23 15:54:59 -0500

    completed testscores.rb

--- testscores.rb
+++ testscores.rb
@@ -3,3 +3,17 @@
 # Print out the scores in ascending order.
 #
 # Find the average of those test scores and print it out.
+
+
+test_scores = [99, 92, 87, 56, 79, 95, 81, 80, 100]
+
+sort = test_scores.sort
+
+puts "test scores from lowest to highest #{sort}"
+
+# average of test scores should be 85, add .to_f to get a float on your answer -> { |sum, el| sum + el }.to_f
+
+avg = test_scores.inject{ |sum, el| sum + el } / test_scores.size
+
+puts "Average test score is #{avg}"
+
