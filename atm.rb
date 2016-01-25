5f88c477289096695c445fc0e2641ca5fdb227a1
Robert Cunningham <rgc3dev@gmail.com>
2016-01-25 11:34:48 -0500

    add homework atm.rb

---
+++ atm.rb
@@ -0,0 +1,7 @@
+# Create an ATM Application
+
+# Create a class called Account
+# Initialize should take on 3 attributes: name, balance, pin
+# Create 4 additional methods: display_balance, withdraw, deposit, and pin_error. 
+# The user should be prompted to enter their pin anytime they call display_balance, withdraw, or deposit.
+# pin_error should contain  "Access denied: incorrect PIN." and be puts when the user types the wrong pin.
