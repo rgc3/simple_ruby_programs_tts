7f61cb355f57cef606a29ec3a33f6bd5d8a05655
Robert Cunningham <rgc3dev@gmail.com>
2016-01-25 08:53:41 -0500

    work on two class assignments on methods

--- reversestringmethod.rb
+++ reversestringmethod.rb
@@ -1,1 +1,13 @@
 # Write a program that takes a String argument and outputs the String in reverse.
+
+# my attempt below
+
+puts "Please enter some text"
+string = gets.chomp
+
+def reverse(x)
+	string = "#{x.reverse}"
+	puts reverse
+end
+
+reverse(x)
--- stringlengthmethod.rb
+++ stringlengthmethod.rb
@@ -1,1 +1,28 @@
 # Write a program that prompts you for a String and then outputs the length of the String.
+
+# My first attempt below
+
+# puts "please enter some text and I will return the length of your string"
+#
+# string = gets.chomp
+#
+# def string_length
+#	length = string.length
+# end
+#
+# puts string_length
+
+
+# Solution below
+	
+puts "please enter some text and I will return the length of your string"
+
+string = gets.chomp
+
+def string_length(x)
+	string = "#{x.length}"
+	puts length
+	
+end
+
+string_length(string)
