7a21c11bcadebf8dcf357335c65670b40de85c5e
Robert Cunningham <rgc3dev@gmail.com>
2016-01-25 08:57:34 -0500

    class assignment reversestringmethod.rb

--- reversestringmethod.rb
+++ reversestringmethod.rb
@@ -1,13 +1,22 @@
 # Write a program that takes a String argument and outputs the String in reverse.
 
-# my attempt below
+# my attempt below, I thought I could do it like the previous problem of stringlengthmethod.rb
 
-puts "Please enter some text"
-string = gets.chomp
+# puts "Please enter some text"
+# string = gets.chomp
+#
+# def reverse(x)
+# 	string = "#{x.reverse}"
+# 	puts reverse
+# end
 
-def reverse(x)
-	string = "#{x.reverse}"
-	puts reverse
+# reverse(x)
+
+
+# solution below
+
+def reverse(string)
+	puts string.reverse
 end
 
-reverse(x)
+reverse('string')
