81f7f10f290c8c160b4130512d42ffbb4d593c8b
Robert Cunningham <rgc3dev@gmail.com>
2016-01-26 14:26:57 -0500

    Start studenttesting.rb, added login

--- studenttesting.rb
+++ studenttesting.rb
@@ -5,3 +5,47 @@
 # Their score should be saved and printed at the end of the test. 
 # If they score under 60 they should be prompted to take the test again. 
 
+class Student
+	def initialize(name, email, password)
+		@name = name
+		@email = email
+		@password = password
+	end
+
+	def name
+		@name
+	end
+
+	def email
+		@email
+	end
+
+	def password
+		@password
+	end
+
+	def login
+		puts 'Please login. What\'s your email?'
+		login_id = gets.chomp.downcase
+		if login_id != @email
+			puts 'Account not found.'
+			login
+		else login_id == @email
+			puts 'Please enter your password.'	
+			pw = gets.chomp.downcase
+			if pw != @password
+				puts 'Access denied.'
+				login
+			else pw == @password
+				puts "Welcome #{@name.capitalize}, you are now ready to take the test."
+			end
+		end
+	end
+end
+
+student_data = Student.new('robert', 'bob@abc.com', 'open')
+
+student_data.login
+
+
+
