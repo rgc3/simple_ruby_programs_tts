462fc44356cfd1cec244ca313770ff8e1b20cee8
Robert Cunningham <rgc3dev@gmail.com>
2016-01-26 11:47:11 -0500

    debug movie.db.rb

--- movie_db.rb
+++ movie_db.rb
@@ -2,7 +2,7 @@
 # Our app will tell you the name, year, director, and rating of a movie.
 # Just make sure to include a class and I'll leave the rest of it up to you.
 
-class Movies
+class Film
 	def initialize(name, year, director, rating)
 		@name = name
 		@year = year
@@ -26,32 +26,26 @@ class Movies
 		@rating
 	end
 
+def movie_info
+		puts 'What movie would you like to look up today?'
+		minfo = gets.chomp.downcase
+		if minfo = @name
+		puts "#{@name}, was directed by #{@director} 
+		in #{@year}, IMDB gives a rating of #{@rating}."
+	else
+		puts 'That movie is not in the database.'
+	end
 end
 
-all_movies = []
+end
+
 
 # ratings are IMDB ratings
 
 movie_data = Film.new('the godfather', 1972, 'francis ford coppola', 9.2)
 
-all_movies.push(movie_data)
 
-movie_data = Film.new('taxi driver', 1976, 'martin scorcese', 8.4)
 
-all_movies.push(movie_data)
 
-movie_data = Film.new('cool hand luke', 1967, 'stuart rosenberg', 8.2)
-
-all_movies.push(movie_data)
-
-def movie_info
-		puts 'What movie would you like to look up today?'
-		minfo = gets.chomp.downcase
-		if minfo = movie
-		puts "#{movie_data.name}, was directed by #{movie_data.director} 
-		in #{movie_data.year}, IMDB gives a rating of #{movie_data.rating}."
-	else
-		puts 'That movie is not in the database.'
-	end
-end
+movie_data.movie_info
 
