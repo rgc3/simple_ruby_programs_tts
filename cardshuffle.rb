f0b6174a437858b9ec688725722d19ac4056a378
Robert Cunningham <rgc3dev@gmail.com>
2016-01-26 14:04:02 -0500

    add cardshuffle.rb and studenttesting.rb

---
+++ cardshuffle.rb
@@ -0,0 +1,4 @@
+# Create a program that will shuffle a deck of cards 
+# and assign a number of players two cards. 
+# Make sure to ask the user for the number of players who are playing.
+
---
+++ studenttesting.rb
@@ -0,0 +1,7 @@
+# Build a student testing app. 
+# The app should have a class of Student and the student should "login" 
+# with their email and password before they can take a test. 
+# The user should be able to take the test and receive feedback on answers. 
+# Their score should be saved and printed at the end of the test. 
+# If they score under 60 they should be prompted to take the test again. 
+
