09f54fdc836eead379b73de54fa0b9b2fb3208dd
Robert Cunningham <rgc3dev@gmail.com>
2016-01-27 08:22:48 -0500

    work on cardshuffle.rb

--- cardshuffle.rb
+++ cardshuffle.rb
@@ -5,8 +5,6 @@
 
 # class Cardgame
 
-
-
 deck = ['ace clubs', '1 clubs', '2 clubs', '3 clubs', '4 clubs', '5 clubs', '6 clubs', '7 clubs', 
 	'8 clubs', '9 clubs', '10 clubs', 'jack clubs', 'queen clubs', 'king clubs', 'ace diamonds', 
 	'2 diamonds', '3 diamonds', '4 diamonds', '5 diamonds', '6 diamonds', '7 diamonds', 
@@ -15,6 +13,8 @@ deck = ['ace clubs', '1 clubs', '2 clubs', '3 clubs', '4 clubs', '5 clubs', '6 c
 	'6 hearts', '7 hearts', '8 hearts', '9 hearts', '10 hearts', 'jack hearts', 'queen hearts', 
 	'king hearts', 'ace spades', '2 spades', '3 spades', '4 spades', '5 spades', '6 spades', 
 	'7 spades', '8 spades', '9 spades', '10 spades', 'jack spades', 'queen spades', 'king spades']
+
+	
 
 p1 = []
 p2 = []
