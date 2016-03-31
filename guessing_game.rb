79d0b07c5bc4e0dad87be843114238a45296b902
Robert Cunningham <rgc3dev@gmail.com>
2016-03-31 09:38:25 -0400

    Amend file for Oh! So close. answer

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,9 +1,10 @@
-randomnumber = rand(100)+1
+# randomnumber = rand(100)+1
+randomnumber = 75
 puts 'Please pick a number from 1 - 100'
 number = gets.chomp.to_i
 if number == randomnumber
 	puts 'Wow! You guessed correctly!'
-elsif number == randomnumber > 5 || randomnumber < 5
+elsif number > 5 || number < 5
 	puts 'Oh! So close.'
 else
 	puts 'Nope! That is not the number.'
